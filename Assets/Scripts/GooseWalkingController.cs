fb74fa70ed55af25ac10d5d4b1a244990f5832b5
BaeYoona <jinalks43@gmail.com>
2025-07-26 15:15:34 +0900

    [Add] 거위 숨는 동작, 환풍구 png 업데이트

--- Assets/Scripts/GooseWalkingController.cs
+++ Assets/Scripts/GooseWalkingController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class GooseWalkingController : MonoBehaviour
 {
     public float moveSpeed = 3f;
-    public float jumpForce = 8f;
+    public float jumpForce = 18f;
     public Transform groundCheck; // 땅 체크용 트랜스폼
     public float groundCheckRadius = 0.1f; // 땅 체크 반경
     public LayerMask groundLayer; // 땅 레이어
@@ -13,6 +13,7 @@ public class GooseWalkingController : MonoBehaviour
     private SpriteRenderer spriteRenderer;
 
     private bool isGrounded;
+    private bool isHiding;
 
     private void Start()
     {
@@ -25,14 +26,22 @@ public class GooseWalkingController : MonoBehaviour
 
     private void Update()
     {
+        // 땅에 있는지 체크
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // 숨기기 키
+        isHiding = Input.GetKey(KeyCode.DownArrow);
+        animator.SetBool("isHiding", isHiding); // 애니메이션 상태 업데이트
+
+
+        if (isHiding)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y); // 이동 중 숨기기 방지
+            return;
+        }
+
         float moveX = Input.GetAxisRaw("Horizontal"); // -1 (왼쪽), 0, 1 (오른쪽)
-
-        // 이동 처리
-        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
-
-        // 애니메이션 처리
-        bool isWalking = Mathf.Abs(moveX) > 0.01f;
-        animator.SetBool("isWalking", isWalking);
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y); // 이동 처리
 
         // 바라보는 방향 처리
         if (moveX != 0)
@@ -40,9 +49,11 @@ public class GooseWalkingController : MonoBehaviour
             spriteRenderer.flipX = moveX < 0; // 왼쪽으로 갈 때 반전
         }
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        // 애니메이션 처리
+        bool isWalking = Mathf.Abs(moveX) > 0.01f;
+        animator.SetBool("isWalking", isWalking);
 
-        if(isGrounded && Input.GetKeyDown(KeyCode.UpArrow))
+        if (isGrounded && Input.GetKeyDown(KeyCode.UpArrow))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
